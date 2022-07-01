3fc08533de359633d362f9a8bf69635db8d792b5
fodare <folorunsodamilare007@hotmail.com>
2022-07-01 18:29:10 +0200

    Add simple hello world console program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// small cosole program to print hello world.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
