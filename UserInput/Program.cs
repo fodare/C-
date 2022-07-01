b4527c5874c8f43d3c8db43431acc16b9616b502
fodare <folorunsodamilare007@hotmail.com>
2022-07-02 01:38:02 +0200

    Add simple user input program

---
+++ UserInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UserInput
+{
+    class USerInputExample
+    {
+        static void Main()
+        {
+            // Tell user what to enter
+            Console.WriteLine("Please enter your name below: ");
+
+            // ReadLine method is used to accept values from user
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age below: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + userName + " you are " + age);
+
+        }
+    }
+}
