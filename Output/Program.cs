ede284de207400e34db38daa04c4fd598fe05a5e
fodare <folorunsodamilare007@hotmail.com>
2022-07-01 19:09:20 +0200

    Small program to test output methods

---
+++ Output/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OutputExample
+{
+    class Output
+    {
+        static void Main(string[] args)
+        {
+            // For every Writeline method call, it prints values in a newlie
+            Console.WriteLine("Hello this");
+            Console.WriteLine("is a ");
+            Console.WriteLine("WriteLine method call");
+
+            // Write method print values in a single line.
+            Console.Write("Hello this is a Write ");
+            Console.Write("method call!");
+        }
+    }
+}
