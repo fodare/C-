50db0f52d3899e12188248ac451c3c8d6e6ed62a
fodare <folorunsodamilare007@hotmail.com>
2022-07-02 23:53:29 +0200

    Simple C# Math methods

---
+++ MathMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathMethods
+{
+    class MathMethodsExample
+    {
+        static void Main()
+        {
+            // C# math methods
+
+            // Max methods
+            int x = 10;
+            int y = 50;
+            Console.WriteLine("The max value of the  two numbers is: " + Math.Max(x, y));
+
+            // Min methods
+            Console.WriteLine("The min value of the two numbers is: " + Math.Min(x, y));
+
+            // Square root method
+            int z = 49;
+            Console.WriteLine("The square root of the number is: " + Math.Sqrt(z));
+
+            // Abosulte method
+            double a = -14.24;
+            Console.WriteLine("The absolute positive value is: " + Math.Abs(a));
+
+            // Round method
+            double b = 20.50;
+            Console.WriteLine("The rounded value of the value is: " + Math.Round(b));
+        }
+    }
+}
