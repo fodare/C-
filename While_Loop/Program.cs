ccb8c4ea62069cbf445eb784734e2000ec21f1f0
fodare <folorunsodamilare007@hotmail.com>
2022-07-07 20:46:06 +0200

    Add sample program for while and do while loop

---
+++ While_Loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WhileLoopExample
+{
+    class WhileExample
+    {
+        static void Main()
+        {
+            int x = 0;
+            // Example of a while loop.
+            while (x < 5)
+            {
+                Console.WriteLine($"{x} is lesser than 5. Incrementing");
+                x++;
+            }
+
+            // Example of do while loop
+
+            bool gameOn = true;
+
+            do
+            {
+                Console.WriteLine("Is the game on. Enter (yes / no ): ");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+                    continue;
+                }
+                else
+                {
+                    gameOn = false;
+                    Console.WriteLine("Thanks. Stopping application!");
+                }
+            } while (gameOn);
+        }
+    }
+}
