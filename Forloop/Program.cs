bbb81cb16cfe03b2ce6233e6ab289a7186ec0f28
fodare <folorunsodamilare007@hotmail.com>
2022-07-09 20:05:30 +0200

    Add simple for loops program

---
+++ Forloop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class ForLoopExamples
+    {
+        static void Main()
+        {
+            // Basic For Loop
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Value of i is: {i}");
+            }
+
+            Console.Clear();
+
+            // Basic foreach loop
+            string[] userNames = { "Mike", "Townsend", "Balake", "John" };
+            foreach (string i in userNames)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
