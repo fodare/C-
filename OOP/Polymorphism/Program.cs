24062a40439edd6433800a7d38738552891d3ce7
fodare <folorunsodamilare007@hotmail.com>
2022-07-21 20:34:55 +0200

    Add intro to Polymorphism

---
+++ OOP/Polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PolymorphismExample
+{
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("This animal makes a sound!");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("Dog barks!");
+        }
+    }
+
+    class Bird : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("Bird sings");
+        }
+
+    }
+
+    class PRogram
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myBird = new Bird();
+
+            myAnimal.animalSound();
+            myBird.animalSound();
+            myDog.animalSound();
+        }
+    }
+}
