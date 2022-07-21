a7e2c992e37719b21a73190ecd0a7d0e3474e798
fodare <folorunsodamilare007@hotmail.com>
2022-07-21 20:13:21 +0200

    Add intro to Inheritance

---
+++ OOP/Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    /*
+    Inheritance is the process of a class deriving it's properties from another class.
+    2 types of categories: Parent class and Derived class.
+    */
+    class Vehicle
+    {
+        public string name;
+
+        public void honk()
+        {
+            Console.WriteLine("Tuut, tuut");
+        }
+    }
+
+    class Car : Vehicle // Inheritance from parent class Vehicle
+    {
+        public string modelName = "Ford Mustang";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+
+            Console.WriteLine(car1.modelName);
+            car1.honk();
+        }
+    }
+}
