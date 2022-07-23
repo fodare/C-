a37c0841ee85193fb3402c764de896afe1a1953f
fodare <folorunsodamilare007@hotmail.com>
2022-07-23 18:44:33 +0200

    Add sample program for file manipulation

---
+++ OOP/Files/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileManipulationExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Creating files and writting message to the file
+            string secretMessage = "Borris Johnson resigned";
+            File.WriteAllText("message.txt", secretMessage);
+            Console.WriteLine("Message written to file successfully!");
+
+            // Reading message from file
+            string extractedMessage = File.ReadAllText("message.txt");
+            Console.WriteLine($"Secret message is: {extractedMessage}");
+        }
+    }
+}
